f728a4aa50b13b3cbe8a8a84107760f07669341c
Skelman50 <skelman50@gmail.com>
2019-12-18 12:18:50 +0200

    first commit

---
+++ src/Column.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import { Droppable } from "react-beautiful-dnd";
+import Task from "./Task";
+
+const TaskList = styled.div`
+  padding: 8px;
+`;
+
+const Title = styled.h3`
+  padding: 8px;
+`;
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgray;
+  border-radius: 2px;
+`;
+
+class Column extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <Droppable droppableId={this.props.column.id}>
+          {provided => (
+            <TaskList {...provided.droppableProps} ref={provided.innerRef}>
+              {this.props.tasks.map((task, index) => (
+                <Task key={task.id} task={task} index={index} />
+              ))}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
+      </Container>
+    );
+  }
+}
+
+export default Column;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "@atlaskit/css-reset";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
